0a8b4e9fc462d656bbe1de8b919be0cbc26662f0
Luiza Marinho <luiza.calheirosm@gmail.com>
2025-02-22 14:09:51 -0300

    Create ex03aula3.c

---
+++ ex03aula3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+
+    printf("Insira um valor inteiro\n");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        printf("%i\n", i);
+    }
+}
